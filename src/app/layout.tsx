aa80941d9f4816d3b66cf644aee4d619036b9870
Maxi <Mrinaldi2204@gmail.com>
2024-11-22 13:32:36 -0300

    New favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,11 @@
 import Header from "@/components/Header";
 import { montserrat } from "@/utils/fonts";
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/react";
 import Footer from "@/components/Footer";
 import { Metadata } from "next";
 import "./globals.css";
 
-export const metadata : Metadata = {
+export const metadata: Metadata = {
   title: {
     default: "Liceo Padre Domingo",
     template: "%s | Liceo Padre Domingo",
@@ -21,6 +21,23 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link
+          rel="icon"
+          type="image/png"
+          href="/favicon.ico/favicon-96x96.png"
+          sizes="96x96"
+        />
+        <link rel="icon" type="image/svg+xml" href="/favicon.ico/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon.ico/apple-touch-icon.png"
+        />
+        <meta name="apple-mobile-web-app-title" content="LPD" />
+        <link rel="manifest" href="/favicon.ico/site.webmanifest" />
+      </head>
       <body className={`${montserrat.className}`}>
         <Header />
         <main>{children}</main>
